1262c5781056eaacbf5c8407622bf9daaab4a26e
Yuan Jin cheng <1411488659@qq.com>
2024-01-04 14:01:49 +0800

    [GeeCache] day3 HTTP服务端

---
+++ GeeCache/day3/GeeCache/geecache.go
@@ -0,0 +1,86 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// Getter loads data from a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// Group is a cache namespace
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group.
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	if g, ok := groups[name]; ok {
+		return g
+	}
+	return nil
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("require a key")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	byte, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(byte)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
